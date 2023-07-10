41f66f89f955825e7af9b8c2b557095e067f08cb
Sivakon Maxim <sivakon.m@yandex.com>
2023-07-10 13:50:21 +0300

    init bitrix default install script

---
+++ public/bitrix/.settings.php
@@ -0,0 +1,71 @@
+<?php
+/* Ansible managed */
+return array (
+  'utf_mode' =>
+  array (
+    'value' => true,
+    'readonly' => true,
+  ),
+  'cache_flags' =>
+  array (
+    'value' =>
+    array (
+      'config_options' => 3600,
+      'site_domain' => 3600,
+    ),
+    'readonly' => false,
+  ),
+  'cookies' =>
+  array (
+    'value' =>
+    array (
+      'secure' => false,
+      'http_only' => true,
+    ),
+    'readonly' => false,
+  ),
+  'exception_handling' =>
+  array (
+    'value' =>
+    array (
+      'debug' => false,
+      'handled_errors_types' => 4437,
+      'exception_errors_types' => 4437,
+      'ignore_silence' => false,
+      'assertion_throws_exception' => true,
+      'assertion_error_type' => 256,
+      'log' => array (
+          'settings' =>
+          array (
+            'file' => '/var/log/php/exceptions.log',
+            'log_size' => 1000000,
+        ),
+      ),
+    ),
+    'readonly' => false,
+  ),
+  'crypto' =>
+  array (
+    'value' =>
+    array (
+        'crypto_key' => 'ecb722a3e7639b9ccf25d8639eab394f',
+    ),
+    'readonly' => true,
+  ),
+  'connections' =>
+  array (
+    'value' =>
+    array (
+      'default' =>
+      array (
+        'className' => '\\Bitrix\\Main\\DB\\MysqliConnection',
+        'host' => 'localhost',
+        'database' => '0N8e4j4DdYBagq1v',
+        'login' => 'F66me0k21ayS07vrzI63',
+        'password' => '5c5Oy7d4lztChRgtnx4X',
+        'options' => 2,
+      ),
+    ),
+    'readonly' => true,
+  )
+);
